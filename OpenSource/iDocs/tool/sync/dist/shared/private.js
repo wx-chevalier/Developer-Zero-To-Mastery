51553b0492a808278f7685ec368460ea316166fd
wxyyxc1992 <384924552@qq.com>
2017-10-11 21:05:39 +0800

    更正 gitignore

--- OpenSource/iDocs/tool/sync/dist/shared/private.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GITHUB_USERNAME = '*';
-exports.GITHUB_PASSWORD = '*';
-exports.WIZ_USERNAME = '*';
-exports.WIZ_PASSWORD = '*';
